3cfe774a63af3e7f6bab6184eeab8bd48ec870ff
vavilen84 <vladimir.teplov@gmail.com>
2021-04-05 16:42:05 +0300

    ADD
    Add testing db

--- shell/docker_sql/create_test_db.sh
+++ shell/docker_sql/create_test_db.sh
@@ -1,1 +1,2 @@
-docker-compose -f docker/dev/docker-compose.yaml --env-file=.env run server go run cli/db/create_test_db.go
+
+docker-compose -f docker/dev/docker-compose.yaml --env-file=.env run db mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_TEST_DATABASE}"
