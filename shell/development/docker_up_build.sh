bba79ebf9afa5fb1d2e5eb9f87f80c483cde8e76
vavilen84 <vladimir.teplov@gmail.com>
2021-04-08 14:56:20 +0300

    REFACTORING
    Docker env refactoring

---
+++ shell/development/docker_up_build.sh
@@ -0,0 +1,2 @@
+sudo chmod 777 -R docker/db/dbdata
+docker-compose -f docker/docker-compose.yaml -f docker/docker-compose.development.yaml --env-file=docker/.env.development up --build
--- shell/docker_up_build.sh
+++
@@ -1,2 +0,0 @@
-sudo chmod 777 -R docker/dev/db/dbdata
-docker-compose -f docker/dev/docker-compose.yaml --env-file=.env up --build
